95da250a487053a319ac63a4da7bcb5e91f6742e
leopuca <leopuca.des@gmail.com>
2023-05-23 12:27:40 +0200

    fixed links

--- utils/GLTFLoader.js
+++ utils/GLTFLoader.js
@@ -62,7 +62,7 @@ import {
 	VectorKeyframeTrack,
 	sRGBEncoding
 } from 'three';
-import { toTrianglesDrawMode } from './utils/BufferGeometryUtils.js';
+import { toTrianglesDrawMode } from './BufferGeometryUtils.js';
 
 class GLTFLoader extends Loader {
 
